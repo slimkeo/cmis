c141b9dc1e3ce7002c7f0ffd0d4a85ad62f3568b
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-02-15 03:55:01 +0200

    dd

--- application/views/backend/burial/navigation.php
+++ application/views/backend/burial/navigation.php
@@ -88,7 +88,7 @@
 
 			<li class="nav-parent <?php
 				if ($page_name == 'members' ||
-						$page_name == 'detailed_meetings' || $pdddage_name == 'detailed_meetings' || $page_name=='beneficiaries')
+						$page_name == 'detailed_meetings' || $page_name == 'detailed_meetings' || $page_name=='beneficiaries')
 					echo 'nav-expanded nav-active';
 				?> ">
 					<a href="#">
