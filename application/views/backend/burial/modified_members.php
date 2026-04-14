a4a567c2c6e4cb1a82de61fc27f662f81d916a58
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-04-14 15:20:46 +0200

    startdate_end_date

--- application/views/backend/burial/modified_members.php
+++ application/views/backend/burial/modified_members.php
@@ -13,18 +13,18 @@
                     <?php echo form_open(base_url() . 'index.php?burial/modified_members_report/', array('class' => 'form-horizontal form-bordered validate', 'target' => '_top')); ?>
 
                     <div class="form-group">
-                        <label class="col-md-3 control-label">Date Range</label>
-                        <div class="col-md-6">
-                            <div class="input-daterange input-group" data-plugin-datepicker>
-                                <span class="input-group-addon">
-                                    <i class="fa fa-calendar"></i>
-                                </span>
-                                <input type="text" class="form-control" name="start_date" value="<?php echo isset($start_date) ? htmlspecialchars($start_date, ENT_QUOTES, 'UTF-8') : ''; ?>" required>
-                                <span class="input-group-addon">To</span>
-                                <input type="text" class="form-control" name="end_date" value="<?php echo isset($end_date) ? htmlspecialchars($end_date, ENT_QUOTES, 'UTF-8') : ''; ?>" required>
-                            </div>
-                        </div>
-                    </div>
+												<label class="col-md-3 control-label">Date Range</label>
+												<div class="col-md-6">
+													<div class="input-daterange input-group" data-plugin-datepicker>
+														<span class="input-group-addon">
+															<i class="fa fa-calendar"></i>
+														</span>
+														<input type="text" class="form-control" name="start_date">
+														<span class="input-group-addon">To</span>
+														<input type="text" class="form-control" name="end_date">
+													</div>
+												</div>
+											</div>
 
                     <div class="form-group">
                         <div class="col-sm-offset-7 col-sm-5">
