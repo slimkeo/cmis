09be34de4b77d89950c9d9dd7c5a396013f40778
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-06-23 12:16:09 +0200

    imigulukudvu

--- application/views/backend/burial/fraud_statement.php
+++ application/views/backend/burial/fraud_statement.php
@@ -43,7 +43,18 @@ $memberName = trim(($recovery->surname ?? '') . ' ' . ($recovery->name ?? ''));
                     <div class="form-group">
                         <label class="col-md-2 control-label">Payment Date</label>
                         <div class="col-md-4">
-                            <input type="date" class="form-control" name="payment_date" required>
+                            <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd">
+                                <input type="text"
+                                    class="form-control"
+                                    name="payment_date"
+                                    pattern="\d{4}-(?:0?[1-9]|1[0-2])-(?:0?[1-9]|[12]\d|3[01])"
+                                    placeholder="yyyy-mm-dd"
+                                    title="Format: yyyy-mm-dd (e.g. 2026-02-17)"
+                                    required>
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-calendar"></i>
+                                </span>
+                            </div>
                         </div>
 
                         <label class="col-md-2 control-label">Amount Paid</label>
@@ -55,7 +66,7 @@ $memberName = trim(($recovery->surname ?? '') . ' ' . ($recovery->name ?? ''));
                     <div class="form-group">
                         <label class="col-md-2 control-label">Payment Method</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control" name="payment_method" placeholder="Cash, Bank, Mobile Money">
+                            <input type="text" class="form-control" value="Bank" name="payment_method" readonly>
                         </div>
 
                         <label class="col-md-2 control-label">Reference No</label>
