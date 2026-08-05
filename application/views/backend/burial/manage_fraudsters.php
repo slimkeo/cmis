1bb8ad1ac91614e5496fc3aebdad94411708953a
Thabani Sicelo Hlanze <sicelohlanze@gmail.com>
2026-08-05 15:19:59 +0200

    scripts

--- application/views/backend/burial/manage_fraudsters.php
+++ application/views/backend/burial/manage_fraudsters.php
@@ -271,3 +271,14 @@ $(document).ready(function() {
     });
 });
 </script>
+<!-- DataTables Buttons CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+
+<!-- Required JS libraries for Excel + PDF + Copy + Print -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
